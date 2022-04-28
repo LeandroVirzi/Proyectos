05d9e71db1506fae31f0f35be2338d7ddbe94c12
leandro virzi <leandro.m.virzi@gmail.com>
2022-04-28 15:47:07 -0300

    Actualización de rutas

--- web-react/client/src/App.js
+++ web-react/client/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.scss';
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
 // as NOMBRE DEL COMPONENTE
 
 function App() {
@@ -9,10 +9,19 @@ function App() {
       <div className="app">
         <h1>Sistema de Rutas Basico</h1>
 
-        <Route exact path="/" component={Home} />
-        <Route exact path="/contact" component={Contact} />
-        <Route exact path="/users" component={Users} />
+        <Link to="/home">Home</Link>
+        <br/>
+        <Link to="/contac">Contact</Link>
+        <br/>
+        <Link to="/users">Users</Link>
+        <br/>
 
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/contact" component={Contact} />
+          <Route exact path="/users" component={Users} />
+          <Route component={Error404} />
+        </Switch>
       </div>
     </Router>
   );
@@ -27,10 +36,14 @@ function Home() {
 
 function Contact() {
   return <h2>Estamos en el componente Contact...</h2>
-}
+};
 
 function Users() {
   return <h2>Estamos en el componente Users</h2>
-}
+};
+
+function Error404 (){
+  return <h2>Error 404...</h2>
+};
 
 export default App;
