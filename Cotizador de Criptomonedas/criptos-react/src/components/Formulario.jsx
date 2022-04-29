95b3c52c472b6d7967aa0a9d2d88356e70a38b3b
leandro virzi <leandro.m.virzi@gmail.com>
2022-04-29 19:49:14 -0300

    actualizacion

--- Cotizador de Criptomonedas/criptos-react/src/components/Formulario.jsx
+++ Cotizador de Criptomonedas/criptos-react/src/components/Formulario.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import styled from '@emotion/styled';
 import useSelectMonedas from '../hooks/useSelectMonedas';
-
+import { monedas } from '../data/monedas';
 
 const InputSubmit = styled.input`
 
@@ -15,6 +15,7 @@ const InputSubmit = styled.input`
     font-size: 20px;
     border-radius: 5px;
     transition: background-color .3s ease;
+    margin-top: 30px;
 
     &:hover {
         background-color: #7A7DFE;
@@ -25,17 +26,15 @@ const InputSubmit = styled.input`
 
 const Formulario = () => {
 
-    const [ SelectMonedas ] = useSelectMonedas('Elige tu Moneda')
-
-    //const [state, setstate] = useState(initialState)
-
-    SelectMonedas();
+    // extraigo de useSelectMonedas el array SelectMonedas
+    const [ SelectMonedas ] = useSelectMonedas('Elige tu Moneda', monedas);
+    //const [ SlectCriptomonedas ] = useSelectMonedas('Elige tu criptomoneda');
+    //SelectMonedas();
 
     return ( 
         <form>
-
             < SelectMonedas />
-
+            
             <InputSubmit 
                 type="submit" 
                 value="Cotizar"
