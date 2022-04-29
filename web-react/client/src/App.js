6678f741a52cc96fdc34a7f3ba4bd5aa6b921b62
leandro virzi <leandro.m.virzi@gmail.com>
2022-04-29 12:16:47 -0300

    Rutas LayoutAdmin

--- web-react/client/src/App.js
+++ web-react/client/src/App.js
@@ -18,9 +18,17 @@ function App() {
   );
 };
 
+// al componente LayoutAdmin le voy a pasar todas las rutas
+// cuando usamos component unicamente rederizamos un unico componente, pero si utilizamos render podemos renderizar varias cosas.
 function RouteWithSubRoutes(route) {
-  console.log(route);
-  return true;
+  //console.log(route);
+  return (
+    <Route
+      path={route.path}
+      exact={route.exact}
+      render={props => <route.component routes={route.routes} {...props} />}
+    />
+  );
 };
  
 
