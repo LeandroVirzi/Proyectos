7cd56ea6ab48ce36e33201facafc2c2047ae3fc3
leandro virzi <leandro.m.virzi@gmail.com>
2022-05-06 01:30:59 -0300

    Actualización sitio web

---
+++ LeandroVirziCV/js/index.js
@@ -0,0 +1,32 @@
+window.sr = ScrollReveal();
+
+    sr.reveal('.navbar', {
+        duration: 3000,
+        origin: 'bottom',
+        distance: '-100px'
+    })
+
+    sr.reveal('.titulo', {
+        duration: 3000,
+        origin: 'bottom',
+        distance: '-100px'
+    })
+
+    sr.reveal('.bottom_scroll', {
+        duration: 2000,
+        origin: 'bottom',
+        distance: '100px'
+    })
+
+    sr.reveal('.der_scroll', {
+        duration: 2000,
+        origin: 'right',
+        distance: '400px'
+    })
+
+    sr.reveal('.izq_scroll', {
+        duration: 2000,
+        origin: 'left',
+        distance: '400px'
+    })
+   
